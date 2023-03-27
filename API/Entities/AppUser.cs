5c7a358beb6f6810ac3a3dcfdc634210522ae986
IsmaelGetronics <ismael.jimenez@getronics.com>
2023-03-27 10:22:38 +0200

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
+using API.Extensions;
 namespace API.Entities
 {
     public class AppUser
@@ -15,5 +10,33 @@ namespace API.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string KnowAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interest { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public List<Photo> Photos { get; set; } = new();
+
+        //public int GetAge(){
+        //    return DateOfBirth.CalculateAge();
+        //}
+
+
     }
 }
